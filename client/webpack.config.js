1eb02323907684a040261b12ff179485c76f05a4
RAFAEL THAYTO TANI <06929749974@jpmorgan.sp>
2018-02-06 12:51:21 -0200

    Update Webpack config and package .json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
+
 module.exports = {
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dir')
+        path: path.resolve(__dirname, 'dist')
     }
 }
